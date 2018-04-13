a8fc4bbfebf86fac439e9340593edbce2e8d99a7
lisa <1432645541@QQ.com>
2018-04-13 20:58:09 +0800

    add js

---
+++ 001.js
@@ -0,0 +1,11 @@
+
+		function skin(){
+			var aa=document.getElementById('bji');
+			 aa.style.backgroundImage='url(../my-html/image/bj1.jpg)';
+		}
+		function exchange(){
+			document.getElementById('box').href='skin1.css'
+		}
+		function incrcla(){
+			document.getElementById('box1').onclick=.className='col1';
+		}
---
+++ 002.js
@@ -0,0 +1,6 @@
+function exchange(){
+			document.getElementById('box').onclick=.href='skin1.css';
+		}
+function incrcla(){
+			document.getElementById('box1').onclick=.className='col1';
+		}
